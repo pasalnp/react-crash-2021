3e21066be87bd64ecb067dc6c8c105cee2d42a99
Pasal.com.np <33492352+pasalnp@users.noreply.github.com>
2022-04-07 16:33:18 +0545

    ✨feature: Doccument Page

---
+++ src/modules/Document/Document.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Document() {
+  return (
+      //TODO: Design a form here with Cheif Complain,Medical History,Allergies,On Examinaiton,Treatment,FollowUp,Image Upload and Save button >> leading to Diagnosis.
+    <div>Document</div>
+  )
+}
